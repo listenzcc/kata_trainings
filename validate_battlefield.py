0f910b91fb65518dda370463ea87fdca4c83e8c0
zcc <listenzcc@mail.bnu.edu.cn>
2019-01-17 15:37:53 +0800

    add quick patch in validate_battlefile

--- validate_battlefield.py
+++ validate_battlefield.py
@@ -43,6 +43,18 @@ def validate_battlefield(battleField):
     return False
 
 
+def validateBattlefield_1(field):
+    return battle(set([(r, c) for c in range(10) for r in range(10) if field[r][c]]), [1, 1, 1, 1, 2, 2, 2, 3, 3, 4])
+
+
+def battle(grid, fleet):
+    return sum(fleet) == len(grid) and (fleet == [] or any(battle(grid - p, fleet[:-1]) for p in possibles(grid, fleet[-1])))
+
+
+def possibles(grid, ship):
+    return [set(p) for p in [[(r+i, c) for i in range(ship)] for r, c in grid] + [[(r, c+i) for i in range(ship)] for r, c in grid] if set(p) <= grid]
+
+
 bf = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
       [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
       [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
