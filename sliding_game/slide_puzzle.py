acf5e0115dd5a53daf36d45b874ded86042f0a9a
zcc <listenzcc@mail.bnu.edu.cn>
2019-03-09 00:38:00 +0800

    add new slide_puzzle

--- sliding_game/slide_puzzle.py
+++ sliding_game/slide_puzzle.py
@@ -6,222 +6,179 @@ class SlidePuzzle():
     def __init__(self, puzzle):
         self.origin = deepcopy(puzzle)
         self.puzzle = deepcopy(puzzle)
-        self.size = (len(puzzle), len(puzzle[0]))
+        self.sz = (len(puzzle), len(puzzle[0]))
         self.mk_ground_truth()
         self.p = self.find(0)
+        self.pnt()
         self.path = []
-        self.path.append(deepcopy(self.puzzle))
 
     def solve(self):
-        self.pnt()
+        # for p in [(0, 0), (0, 1), (0, 2), (0, 3)]:
         p = self.check()
+        while p:
+            if p[0] > self.sz[0]-3:
+                break
+            if p[1] == self.sz[1]-1:
+                self.move(self.gt[p], (p[0]+1, p[1]))
+                self.move_v2(p[0])
+            else:
+                self.move(self.gt[p], p)
+            p = self.check()
 
-        while self.check(self.block):
-            p = self.check(self.block)
-            self.position_v1(p)
+        for p in [(self.sz[0]-2, j) for j in range(self.sz[1]-2)]:
+            self.move(self.gt[p], p)
+            if self.gt[(p[0]+1, p[1])] == self.get((p[0]+1, p[1])):
+                continue
+            self.move(self.gt[(p[0]+1, p[1])], (p[0], p[1]+1))
+            self.move_v3(p[1])
 
-        for j in range(self.size[0]-2):
-            self.position_v1((j, self.size[1]-2))
-            self.position_v2((j, self.size[1]-1))
+        self.goto((self.sz[0]-1, self.sz[1]-1))
 
-        for j in range(self.size[1]-2):
-            self.position_v1((self.size[0]-2, j))
-            self.position_v3((self.size[0]-1, j))
-
-        self.goto((self.size[0]-1, self.size[1]-1))
-
-        while self.check([]):
+        for j in range(4):
             self.up()
             self.left()
             self.down()
             self.right()
+            if not self.check():
+                print('done.')
+                return
 
-        return self.path
-
-    def position_v3(self, dp):
-        print('working:', dp, self.gt[dp])
-        if not self.p[1] == self.size[1]-1:
-            self.right()
-        if self.get(dp) == self.gt[dp]:
-            return
-        cp = self.find(self.gt[dp])
-
-        # move cp to bottom, blank stops at cp's top
-        if cp[0] == self.p[0]:
-            if cp[0] == self.size[0]-1:
-                self.up()
-            else:
-                self.down()
-        self.goto((self.p[0], cp[1]))
-        if not cp[0] == self.size[0]-1:
-            self.up()
-        cp = self.find(self.gt[dp])
-
-        # move cp left two 2nd column
-        while cp[1] > dp[1]+1:
+        for j in range(4):
             self.left()
-            self.down()
-            self.right()
-            self.up()
-            self.left()
-            cp = self.find(self.gt[dp])
-
-        self.right()
-        self.down()
-        self.left()
-        self.up()
-        self.left()
-        self.down()
-        self.right()
-        self.up()
-        self.left()
-        self.down()
-        self.right()
-        self.right()
-        self.up()
-        self.left()
-        self.left()
-        self.down()
-        self.right()
-
-    def position_v2(self, dp):
-        print('working:', dp, self.gt[dp])
-        self.down()
-        if self.get(dp) == self.gt[dp]:
-            return
-        cp = self.find(self.gt[dp])
-
-        # move cp to right end, blank stops at cp's left
-        if not self.size[1] - cp[1] in [1, 2]:
-            if cp[0] == self.size[0]-1:
-                self.goto((cp[0]-1, cp[1]))
-                self.down()
-            else:
-                self.goto((cp[0]+1, cp[1]))
-            while cp[1] < dp[1]:
-                self.right()
-                self.up()
-                self.left()
-                cp = self.find(self.gt[dp])
-                if cp[1] == dp[1]:
-                    break
-                self.down()
-                self.right()
-
-        # blank stops at cp's left
-        if cp[1] == self.p[1]:
-            if cp[1] == self.size[1]-1:
-                self.left()
-            else:
-                self.right()
-        self.goto((cp[0], self.p[1]))
-        if cp[1] == self.size[1]-2:
-            self.left()
-        cp = self.find(self.gt[dp])
-
-        # move cp up to 2nd row
-        while cp[0] > dp[0]+1:
             self.up()
             self.right()
             self.down()
-            self.left()
-            self.up()
-            cp = self.find(self.gt[dp])
+            if not self.check():
+                print('done.')
+                return
 
+        return
+
+    def move_v3(self, c):
+        print('settling:',
+              self.get((self.sz[0]-2, 0)), self.get((self.sz[0]-2, 1)))
+
+        # move blank to x's down
+        self.goto((self.sz[0]-1, c+1))
+
+        # fit last 2 rows
+        self.up()
+        self.right()
+        self.down()
+        self.left()
+        self.left()
+        self.up()
+        self.right()
         self.down()
         self.right()
         self.up()
-        self.up()
+        self.left()
         self.left()
         self.down()
         self.right()
-        self.down()
-        self.left()
-        self.up()
-        self.up()
-        self.right()
-        self.down()
 
-    def position_v1(self, dp):
-        print('working:', dp, self.gt[dp])
-        if self.get(dp) == self.gt[dp]:
+    def move_v2(self, r):
+        print('settling:',
+              self.get((r, self.sz[1]-2)), self.get((r+1, self.sz[1]-1)))
+        if self.p[0] == r:
+            self.down()
             return
-        cp = self.find(self.gt[dp])
 
-        # move blank to cp's down
-        if cp[1] == self.p[1]:
-            self.goto(cp)
-        else:
-            if cp[0] == self.size[0]-1:
-                self.goto((cp[0]-1, cp[1]))
+        # move blank to x's down
+        self.goto((r+2, self.sz[1]-1))
+
+        # fit last 2 columns
+        self.up()
+        self.left()
+        self.up()
+        self.right()
+        self.down()
+        self.left()
+        self.up()
+        self.right()
+        self.down()
+        self.down()
+        self.left()
+        self.up()
+        self.up()
+        self.right()
+        self.down()
+
+    def move(self, x, dest):
+        print('moving:', x, dest)
+        if self.get(dest) == x:
+            return
+
+        # goto x as init
+        self.goto(self.find(x))
+
+        # make sure blank at x's down
+        cp = self.find(x)
+        if not cp[0] == self.p[0]-1:
+            if cp[0] == self.p[0]+1:
                 self.down()
             else:
-                self.goto((cp[0]+1, cp[1]))
-        cp = self.find(self.gt[dp])
+                if cp[0] == self.sz[0]-1:
+                    self.up()
+                    self.goto((cp[0]-1, cp[1]))
+                    self.down()
+                else:
+                    self.down()
+                    self.goto((cp[0]+1, cp[1]))
 
-        # fit column, blank stops at cp's down
+        # fit column, blank stops at x's down
+        cp = self.find(x)
         # right
-        while cp[1] < dp[1]:
+        while cp[1] < dest[1]:
             self.right()
             self.up()
             self.left()
             self.down()
             self.right()
-            cp = self.find(self.gt[dp])
-            # if cp[1] == dp[1]:
-            #    break
+            cp = self.find(x)
         # left
-        while cp[1] > dp[1]:
+        while cp[1] > dest[1]:
             self.left()
             self.up()
             self.right()
             self.down()
             self.left()
-            cp = self.find(self.gt[dp])
-            # if cp[1] == dp[1]:
-            #    break
-
-        self.right()
-        self.up()
+            cp = self.find(x)
 
         # fit row
-        # down
-        while cp[0] < dp[0]:
-            self.down()
+        cp = self.find(x)
+        if cp[1] == self.sz[1]-1:
             self.left()
             self.up()
-            if cp[0] == dp[0]:
-                break
-            self.right()
-            self.down()
-            cp = self.find(self.gt[dp])
-        # up
-        while cp[0] > dp[0]:
-            self.up()
-            self.left()
-            self.down()
-            if cp[0] == dp[0]:
-                break
+            while cp[0] > dest[0]:
+                self.up()
+                self.right()
+                self.down()
+                self.left()
+                self.up()
+                cp = self.find(x)
+        else:
             self.right()
             self.up()
-            cp = self.find(self.gt[dp])
+            while cp[0] > dest[0]:
+                self.up()
+                self.left()
+                self.down()
+                self.right()
+                self.up()
+                cp = self.find(x)
 
     def mk_ground_truth(self):
         self.gt = dict()
-        self.block = set()
         x = 1
-        for j in range(self.size[0]):
-            for k in range(self.size[1]):
+        for j in range(self.sz[0]):
+            for k in range(self.sz[1]):
                 self.gt[(j, k)] = x
                 x += 1
-                if k > self.size[1]-3 or j > self.size[0]-3:
-                    self.block.add((j, k))
         self.gt[(j, k)] = 0
 
-    def check(self, block=[]):
+    def check(self):
         for p in self.gt.keys():
-            if p in block:
-                continue
             if not self.get(p) == self.gt[p]:
                 return p
         return 0
@@ -260,8 +217,6 @@ class SlidePuzzle():
         self.set(p_, 0)
         self.p = p_
         self.pnt()
-        self.path.append(deepcopy(self.puzzle))
-        print(self.path)
 
     def left(self):
         self.step((0, -1))
@@ -276,29 +231,30 @@ class SlidePuzzle():
         self.step((1, 0))
 
 
-puzzle1 = [
+puzzle3 = [
     [4, 1, 3],
     [2, 8, 0],
     [7, 6, 5]
 ]
-puzzle11 = [[8, 2, 1], [3, 7, 0], [4, 6, 5]]
-puzzle2 = [
+puzzle31 = [
+    [8, 2, 1],
+    [3, 7, 0],
+    [4, 6, 5]
+]
+puzzle4 = [
     [10, 3, 6, 4],
     [1, 5, 8, 0],
     [2, 13, 7, 15],
     [14, 9, 12, 11]
 ]
-puzzle3 = [
+puzzle5 = [
     [3, 7, 14, 15, 10],
     [1, 0, 5, 9, 4],
     [16, 2, 11, 12, 8],
     [17, 6, 13, 18, 20],
     [21, 22, 23, 19, 24]
 ]
-board = [
-    [4, 1, 2],
-    [5, 0, 3]
-]
+
 simpleExample = [
     [1, 2, 3, 4],
     [5, 0, 6, 8],
@@ -306,5 +262,5 @@ simpleExample = [
     [13, 14, 15, 12]
 ]
 
-sp = SlidePuzzle(puzzle11)
+sp = SlidePuzzle(puzzle4)
 sp.solve()
